2989d90f93e50f73a32d8dde263e26c66cacaea9
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2022-10-11 19:32:28 +0530

    Time: 17 ms (12.06%), Space: 44.7 MB (30.17%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i+=3){
+            if(nums.length==1){
+                return nums[0];
+            }
+            else if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
