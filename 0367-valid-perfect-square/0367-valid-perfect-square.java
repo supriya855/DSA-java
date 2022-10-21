6d0dda50240cfe5a311281b50a538037ce631825
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2022-10-21 19:16:08 +0530

    Time: 2 ms (13.16%), Space: 41.4 MB (13.24%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+       for(int i=1;i<=Math.sqrt(num);i++){
+           if(i*i==num){
+               return true;
+           }
+       }
+        
+        return false;
+        
+    }
+}
