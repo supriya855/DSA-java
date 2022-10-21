1de3d77879911155fd4910f2ee800be85beed3a1
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2022-10-21 18:35:21 +0530

    Time: 0 ms (100.00%), Space: 73.2 MB (41.03%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,5 +1,7 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
+  /*
+  This is the brute force approach time complexity is O(n)
   int max=0,c=0;
         for(int i=0;i<arr.length;i++){
             if(max<arr[i]){
@@ -7,28 +9,19 @@ class Solution {
                 c=i;
             }
         }
-        return c;
-//     int s=0;
-//     int e=arr.length-1;
-//     int mid=s+(e-s)/2;
-     
-//         for(int i=0;i<=mid;i++){
-//             if(arr[i]<=arr[mid]){
-//                 s++;
-//             }
-//             else{
-//                 mid=s;
-//             }
-//         }
-//         for(int j=e;j>=mid;j--){
-//             if(arr[j]<=arr[mid]){
-//                 e--;
-//             }
-//             else{
-//                 mid=e;
-//             }
-//         }
-        
-//         return mid;
+        return c;*/
+    int s=0;
+    int e=arr.length-1;
+        while(s<e){
+    int mid=s+(e-s)/2;
+            if(arr[mid]>arr[mid+1]){
+                e=mid;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return s;
     }
 }
+       
