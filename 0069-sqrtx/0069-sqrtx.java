40fa700edc215654e7ac5ecf8f66b5016ed0a73b
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2022-10-22 23:02:29 +0530

    Time: 2 ms (84.60%), Space: 41.4 MB (50.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-       return (int)Math.sqrt(x);
+
+     if(x <= 0) return 0;
+        int l = 1, r = x, res = 1;
+        while(l < r) {
+            int mid = (l + r) / 2;
+            if(mid > x/mid) {
+                r = mid;
+            } else {
+                res = mid;
+                l = mid + 1;
+            }
+        }
+        return res;
     }
 }
