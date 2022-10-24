05960c5c376e017e21dea436045ac846d10dff41
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2022-10-24 23:25:00 +0530

    Time: 1 ms (100.00%), Space: 51.2 MB (28.42%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,22 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       // HashMap<Integer,Integer> list= new HashMap<Integer,Integer>();
-     int n=nums.length;
-        Arrays.sort(nums);
- for(int i=0;i<nums.length-1;i+=2){
-     if(n==1){
-         return nums[0];
-     }
-     else if(nums[i]!=nums[i+1]){
-         return nums[i];
-     }
- }       
+//        // HashMap<Integer,Integer> list= new HashMap<Integer,Integer>();
+//      int n=nums.length;
+//         Arrays.sort(nums);
+//  for(int i=0;i<nums.length-1;i+=2){
+//      if(n==1){
+//          return nums[0];
+//      }
+//      else if(nums[i]!=nums[i+1]){
+//          return nums[i];
+//      }
+//  }       
         
-    return nums[nums.length-1];
+//     return nums[nums.length-1];
+        int unique=0;
+        for(int n:nums){
+            unique^=n;
+        }
+        return unique;
     }
 }
