1076a0e6945069e879e5721e2353fee09667a47f
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2022-10-29 20:40:08 +0530

    Time: 7 ms (96.06%), Space: 44.9 MB (98.66%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       if(strs==null||strs.length==0) return new ArrayList<>();
+        Map<String,List<String>> map= new HashMap<>();
+        for(String s:strs){
+            char[] ch= s.toCharArray();
+            Arrays.sort(ch);
+            String key=String.valueOf(ch);
+            if(!map.containsKey(key)) map.put(key,new ArrayList<>());
+            map.get(key).add(s);
+        }
+       return new ArrayList<>(map.values()); 
+    }
+     // if (strs == null || strs.length == 0) return new ArrayList<>();
+     //    Map<String, List<String>> map = new HashMap<>();
+     //    for (String s : strs) {
+     //        char[] ca = s.toCharArray();
+     //        Arrays.sort(ca);
+     //        String keyStr = String.valueOf(ca);
+     //        if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+     //        map.get(keyStr).add(s);
+     //    }
+        //return new ArrayList<>(map.values());
+}
