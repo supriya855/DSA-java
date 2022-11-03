3201f7832b19caf7212883486688df5f9604c6e5
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2022-11-04 00:15:32 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (7.55%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,13 +1,21 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    c++;
-                }
-            }
+        //Brute force approach with O(N2) complexity
+        // int c=0;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
+        int res=0;
+        int [] c= new int[101];
+        for(int n:nums){
+            res+=c[n]++;
+            
         }
-        return c;
+        return res;
     }
 }
