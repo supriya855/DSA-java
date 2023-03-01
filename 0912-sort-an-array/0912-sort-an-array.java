faa25be3cde233ac08dcdf35a463281d31b1c01c
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-03-01 17:54:03 +0530

    Time: 41 ms (43.73%), Space: 52.6 MB (58.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,8 +1,39 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        return mergeSort(nums);
     }
-  
-    
+    public int[] mergeSort(int[] nums){
+        if(nums.length==1) return nums;
+        int mid=nums.length/2;
+        int[] left=mergeSort(Arrays.copyOfRange(nums,0,mid));
+        int[] right=mergeSort(Arrays.copyOfRange(nums,mid,nums.length));
+        return merge(left,right);
+    }
+    public int[] merge(int[] left,int[] right){
+        int[] mix= new int[left.length+right.length];
+        int i=0,j=0,k=0;
+        while(i<left.length&&j<right.length){
+            if(left[i]<right[j]){
+                mix[k]=left[i];
+                i++;
+            }
+            else{
+                mix[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<left.length){
+            mix[k]=left[i];
+            i++;
+            k++;
+        }
+        while(j<right.length){
+            mix[k]=right[j];
+            j++;
+            k++;
+        }
+        return mix;
+        
+    }
 }
