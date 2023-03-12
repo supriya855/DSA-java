d6d9b6eead34466e299cd64a85a212a401441b99
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-03-12 09:46:30 +0530

    Time: 32 ms (100.00%), Space: 52 MB (100.00%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
@@ -1,22 +1,15 @@
 class Solution {
     public int maxScore(int[] nums) {
+        int count=0;
         Arrays.sort(nums);
         long[] res = new long[nums.length];
-        int[] temp=new int[nums.length];
-        for(int k=nums.length-1;k>=0;k--){
-            temp[nums.length-k-1]=nums[k];
+        long sum=0;
+        for(int i=nums.length-1;i>=0;i--){
+            sum+=nums[i];
+            res[i]=sum;
+            if(res[i]>0) count++;
         }
-        long count=0;
-        res[0]=temp[0];
-        if(res[0]>0) count++;
-        for(int i=1;i<nums.length;i++){
-            res[i]=res[i-1]+temp[i];
-            if(res[i]>0){
-               count++;
-           }
-        }
-        System.out.println(Arrays.toString(res));
+        return count;
        
-        return (int)count;
     }
 }
