981395413b0139ff76ffa06c6e0399ef072e74c1
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-03-12 07:40:38 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (14.60%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -13,10 +13,9 @@ class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null&&fast.next!=null){
-            slow=slow.next;
             fast=fast.next.next;
+            slow=slow.next;
         }
         return slow;
-        
     }
 }
