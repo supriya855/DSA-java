45e66e09fa7210a1c73a645b152c508e6a349907
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2023-04-05 17:05:43 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -41,35 +41,46 @@ class GFG {
 // } Driver Code Ends
 
 
+
 //User function Template for Java
 
 
 class Solution {
-    // This is using merge Sort
-    public long kthElement( int arr1[], int arr2[], int n, int m, int l) {
-       int[] arr= new int[n+m];
-       int i=0,j=0,k=0;
-       while(i<n&&j<m){
-           if(arr1[i]<arr2[j]){
-               arr[k]=arr1[i];
-               i++;
-           }
-          else{
-               arr[k]=arr2[j];
-               j++;
-           }
-           k++;
-       }
-       while(i<n&&k<n+m){
-           arr[k]=arr1[i];
-           i++;
-           k++;
-       }
-         while(j<m&&k<n+m){
-           arr[k]=arr2[j];
-           j++;
-           k++;
-       }
-        return arr[l-1];
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        if(n>m)
+         return kthElement(arr2,arr1,m,n,k);
+         int low=Math.max(0,k-m) , high=Math.min(k,n);
+         while(low<=high){
+             int cutLeft=(low+high)>>1;
+             int cutRight=k-cutLeft;
+             int l1=cutLeft<=0?Integer.MIN_VALUE:arr1[cutLeft-1];
+             int l2=cutRight<=0?Integer.MIN_VALUE:arr2[cutRight-1];
+             int r1=cutLeft==n?Integer.MAX_VALUE:arr1[cutLeft];
+             int r2=cutRight==m?Integer.MAX_VALUE:arr2[cutRight];
+             if(l1<=r2&&l2<=r1){
+                 return Math.max(l1,l2);
+             }
+             else if(l1>r2){
+                 high=cutLeft-1;
+             }
+             else{
+                 low=cutLeft+1;
+             }
+         }
+         return 1;
     }
+    // This is using binary search 
+    // Assume 
+    /*  2 3 6 7 9
+        1 4 8 10
+        for this example after sorted whole array
+                1 2 3 4 6|7 9 10
+                         |( i.e is cut)
+        cutLeft             cutRight
+        arr1:  2 3 6        7 9
+        arr2 : 1 4          8 10
+        left part of arr1 i.e l1<=r2
+                     arr2: l2<=r1
+    */
+    
 }
